d16f6bd163638e73b4122e433036bd7e33fb0a6e
thinkingmore <peterkevin823@gmail.comm>
2022-12-27 11:41:04 +0600

    setup the database connection

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,6 +11,25 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+// connect database
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ltn8juo.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    try {
+
+        const taskCollection = client.db('taskManager').collection('taskCollection');
+
+    }
+    finally {
+
+    }
+}
+
 
 app.get('/', ( req, res) => {
     res.send('Task Manger Server is running')
